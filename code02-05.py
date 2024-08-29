a5190ee6b4c90b235e0a49f5fe40a39d3415b326
rlaalstj902 <162665482+rlaalstj902@users.noreply.github.com>
2024-08-29 10:24:40 +0900

    Add files via upload

---
+++ code02-05.py
@@ -0,0 +1,14 @@
+#code02-05.py
+import turtle
+
+turtle.shape('turtle')
+
+turtle.forward(200)
+turtle.right(90)
+turtle.foward(200)
+turtle.right(90)
+turtle.foward(200)
+turtle.right(90)
+turtle.foward(200)
+
+turtle.done()
