e5f03f26da03ac3df2e79e586661e3bd490ada21
rlaalstj902 <162665482+rlaalstj902@users.noreply.github.com>
2024-09-06 23:59:08 +0900

    Add files via upload

---
+++ code10-05.py
@@ -0,0 +1,12 @@
+#code10-05.py
+
+from tkinter import *
+
+window = Tk()
+
+button1 = Button(window, text ="파이썬 종료", fg="red",
+                 command = quit)
+
+button1.pack()
+
+window.mainloop()
